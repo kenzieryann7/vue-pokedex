d9049c6cc20a9ddc60c8fec6d8e4d0f1e26c821a
kenzieryann7 <makenzie_99@hotmail.com>
2021-11-28 14:14:24 -0800

    Adding more content to the modal

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -179,6 +179,10 @@
                     <td>Speed</td>
                     <td>{{ selectedPokemon?.speed }}</td>
                   </tr>
+                  <tr class="fw-bold">
+                    <td>Total</td>
+                    <td>{{ selectedPokemon?.total }}</td>
+                  </tr>
                 </tbody>
               </table>
             </div>
@@ -186,61 +190,185 @@
               <div>
                 <span class="fw-bold">Type</span>
                 <br />
-                <span
-                  class="badge me-2"
-                  :class="{
-                    normal: selectedPokemon?.type1 == 'Normal',
-                    fire: selectedPokemon?.type1 == 'Fire',
-                    water: selectedPokemon?.type1 == 'Water',
-                    grass: selectedPokemon?.type1 == 'Grass',
-                    electric: selectedPokemon?.type1 == 'Electric',
-                    ice: selectedPokemon?.type1 == 'Ice',
-                    fighting: selectedPokemon?.type1 == 'Fighting',
-                    poison: selectedPokemon?.type1 == 'Poison',
-                    ground: selectedPokemon?.type1 == 'Ground',
-                    flying: selectedPokemon?.type1 == 'Flying',
-                    psychic: selectedPokemon?.type1 == 'Psychic',
-                    bug: selectedPokemon?.type1 == 'Bug',
-                    rock: selectedPokemon?.type1 == 'Rock',
-                    ghost: selectedPokemon?.type1 == 'Ghost',
-                    dragon: selectedPokemon?.type1 == 'Dragon',
-                    dark: selectedPokemon?.type1 == 'Dark',
-                    steel: selectedPokemon?.type1 == 'Steel',
-                    fairy: selectedPokemon?.type1 == 'Fairy'
-                  }"
-                  >{{ selectedPokemon?.type1 }}</span
-                >
-                <span
-                  class="badge"
-                  :class="{
-                    normal: selectedPokemon?.type2 == 'Normal',
-                    fire: selectedPokemon?.type2 == 'Fire',
-                    water: selectedPokemon?.type2 == 'Water',
-                    grass: selectedPokemon?.type2 == 'Grass',
-                    electric: selectedPokemon?.type2 == 'Electric',
-                    ice: selectedPokemon?.type2 == 'Ice',
-                    fighting: selectedPokemon?.type2 == 'Fighting',
-                    poison: selectedPokemon?.type2 == 'Poison',
-                    ground: selectedPokemon?.type2 == 'Ground',
-                    flying: selectedPokemon?.type2 == 'Flying',
-                    psychic: selectedPokemon?.type2 == 'Psychic',
-                    bug: selectedPokemon?.type2 == 'Bug',
-                    rock: selectedPokemon?.type2 == 'Rock',
-                    ghost: selectedPokemon?.type2 == 'Ghost',
-                    dragon: selectedPokemon?.type2 == 'Dragon',
-                    dark: selectedPokemon?.type2 == 'Dark',
-                    steel: selectedPokemon?.type2 == 'Steel',
-                    fairy: selectedPokemon?.type2 == 'Fairy'
-                  }"
-                  >{{ selectedPokemon?.type2 }}</span
-                >
+                <h5>
+                  <span
+                    class="badge me-2"
+                    :class="{
+                      normal: selectedPokemon?.type1 == 'Normal',
+                      fire: selectedPokemon?.type1 == 'Fire',
+                      water: selectedPokemon?.type1 == 'Water',
+                      grass: selectedPokemon?.type1 == 'Grass',
+                      electric: selectedPokemon?.type1 == 'Electric',
+                      ice: selectedPokemon?.type1 == 'Ice',
+                      fighting: selectedPokemon?.type1 == 'Fighting',
+                      poison: selectedPokemon?.type1 == 'Poison',
+                      ground: selectedPokemon?.type1 == 'Ground',
+                      flying: selectedPokemon?.type1 == 'Flying',
+                      psychic: selectedPokemon?.type1 == 'Psychic',
+                      bug: selectedPokemon?.type1 == 'Bug',
+                      rock: selectedPokemon?.type1 == 'Rock',
+                      ghost: selectedPokemon?.type1 == 'Ghost',
+                      dragon: selectedPokemon?.type1 == 'Dragon',
+                      dark: selectedPokemon?.type1 == 'Dark',
+                      steel: selectedPokemon?.type1 == 'Steel',
+                      fairy: selectedPokemon?.type1 == 'Fairy'
+                    }"
+                    >{{ selectedPokemon?.type1 }}</span
+                  >
+                  <span
+                    class="badge"
+                    :class="{
+                      normal: selectedPokemon?.type2 == 'Normal',
+                      fire: selectedPokemon?.type2 == 'Fire',
+                      water: selectedPokemon?.type2 == 'Water',
+                      grass: selectedPokemon?.type2 == 'Grass',
+                      electric: selectedPokemon?.type2 == 'Electric',
+                      ice: selectedPokemon?.type2 == 'Ice',
+                      fighting: selectedPokemon?.type2 == 'Fighting',
+                      poison: selectedPokemon?.type2 == 'Poison',
+                      ground: selectedPokemon?.type2 == 'Ground',
+                      flying: selectedPokemon?.type2 == 'Flying',
+                      psychic: selectedPokemon?.type2 == 'Psychic',
+                      bug: selectedPokemon?.type2 == 'Bug',
+                      rock: selectedPokemon?.type2 == 'Rock',
+                      ghost: selectedPokemon?.type2 == 'Ghost',
+                      dragon: selectedPokemon?.type2 == 'Dragon',
+                      dark: selectedPokemon?.type2 == 'Dark',
+                      steel: selectedPokemon?.type2 == 'Steel',
+                      fairy: selectedPokemon?.type2 == 'Fairy'
+                    }"
+                    >{{ selectedPokemon?.type2 }}</span
+                  >
+                </h5>
               </div>
               <br />
               <div class="mb-3">
                 <span class="fw-bold">Weaknesses</span>
+                <br />
+                <!-- WEAKNESS 1 -->
+                <h5>
+                  <span
+                    class="badge bg-lg me-2"
+                    :class="{
+                      normal: selectedPokemon?.weak1 == 'Normal',
+                      fire: selectedPokemon?.weak1 == 'Fire',
+                      water: selectedPokemon?.weak1 == 'Water',
+                      grass: selectedPokemon?.weak1 == 'Grass',
+                      electric: selectedPokemon?.weak1 == 'Electric',
+                      ice: selectedPokemon?.weak1 == 'Ice',
+                      fighting: selectedPokemon?.weak1 == 'Fighting',
+                      poison: selectedPokemon?.weak1 == 'Poison',
+                      ground: selectedPokemon?.weak1 == 'Ground',
+                      flying: selectedPokemon?.weak1 == 'Flying',
+                      psychic: selectedPokemon?.weak1 == 'Psychic',
+                      bug: selectedPokemon?.weak1 == 'Bug',
+                      rock: selectedPokemon?.weak1 == 'Rock',
+                      ghost: selectedPokemon?.weak1 == 'Ghost',
+                      dragon: selectedPokemon?.weak1 == 'Dragon',
+                      dark: selectedPokemon?.weak1 == 'Dark',
+                      steel: selectedPokemon?.weak1 == 'Steel',
+                      fairy: selectedPokemon?.weak1 == 'Fairy'
+                    }"
+                    >{{ selectedPokemon?.weak1 }}</span
+                  >
+
+                  <!-- WEAKNESS 2 -->
+                  <span
+                    class="badge me-2"
+                    :class="{
+                      normal: selectedPokemon?.weak2 == 'Normal',
+                      fire: selectedPokemon?.weak2 == 'Fire',
+                      water: selectedPokemon?.weak2 == 'Water',
+                      grass: selectedPokemon?.weak2 == 'Grass',
+                      electric: selectedPokemon?.weak2 == 'Electric',
+                      ice: selectedPokemon?.weak2 == 'Ice',
+                      fighting: selectedPokemon?.weak2 == 'Fighting',
+                      poison: selectedPokemon?.weak2 == 'Poison',
+                      ground: selectedPokemon?.weak2 == 'Ground',
+                      flying: selectedPokemon?.weak2 == 'Flying',
+                      psychic: selectedPokemon?.weak2 == 'Psychic',
+                      bug: selectedPokemon?.weak2 == 'Bug',
+                      rock: selectedPokemon?.weak2 == 'Rock',
+                      ghost: selectedPokemon?.weak2 == 'Ghost',
+                      dragon: selectedPokemon?.weak2 == 'Dragon',
+                      dark: selectedPokemon?.weak2 == 'Dark',
+                      steel: selectedPokemon?.weak2 == 'Steel',
+                      fairy: selectedPokemon?.weak2 == 'Fairy'
+                    }"
+                    >{{ selectedPokemon?.weak2 }}</span
+                  >
+                  <!-- WEAKNESS 3 -->
+                  <span
+                    class="badge me-2"
+                    :class="{
+                      normal: selectedPokemon?.weak3 == 'Normal',
+                      fire: selectedPokemon?.weak3 == 'Fire',
+                      water: selectedPokemon?.weak3 == 'Water',
+                      grass: selectedPokemon?.weak3 == 'Grass',
+                      electric: selectedPokemon?.weak3 == 'Electric',
+                      ice: selectedPokemon?.weak3 == 'Ice',
+                      fighting: selectedPokemon?.weak3 == 'Fighting',
+                      poison: selectedPokemon?.weak3 == 'Poison',
+                      ground: selectedPokemon?.weak3 == 'Ground',
+                      flying: selectedPokemon?.weak3 == 'Flying',
+                      psychic: selectedPokemon?.weak3 == 'Psychic',
+                      bug: selectedPokemon?.weak3 == 'Bug',
+                      rock: selectedPokemon?.weak3 == 'Rock',
+                      ghost: selectedPokemon?.weak3 == 'Ghost',
+                      dragon: selectedPokemon?.weak3 == 'Dragon',
+                      dark: selectedPokemon?.weak3 == 'Dark',
+                      steel: selectedPokemon?.weak3 == 'Steel',
+                      fairy: selectedPokemon?.weak3 == 'Fairy'
+                    }"
+                    >{{ selectedPokemon?.weak3 }}</span
+                  >
+                  <!-- WEAKNESS 4 -->
+                  <span
+                    class="badge"
+                    :class="{
+                      normal: selectedPokemon?.weak4 == 'Normal',
+                      fire: selectedPokemon?.weak4 == 'Fire',
+                      water: selectedPokemon?.weak4 == 'Water',
+                      grass: selectedPokemon?.weak4 == 'Grass',
+                      electric: selectedPokemon?.weak4 == 'Electric',
+                      ice: selectedPokemon?.weak4 == 'Ice',
+                      fighting: selectedPokemon?.weak4 == 'Fighting',
+                      poison: selectedPokemon?.weak4 == 'Poison',
+                      ground: selectedPokemon?.weak4 == 'Ground',
+                      flying: selectedPokemon?.weak4 == 'Flying',
+                      psychic: selectedPokemon?.weak4 == 'Psychic',
+                      bug: selectedPokemon?.weak4 == 'Bug',
+                      rock: selectedPokemon?.weak4 == 'Rock',
+                      ghost: selectedPokemon?.weak4 == 'Ghost',
+                      dragon: selectedPokemon?.weak4 == 'Dragon',
+                      dark: selectedPokemon?.weak4 == 'Dark',
+                      steel: selectedPokemon?.weak4 == 'Steel',
+                      fairy: selectedPokemon?.weak4 == 'Fairy'
+                    }"
+                    >{{ selectedPokemon?.weak4 }}</span
+                  >
+                </h5>
               </div>
               <div>
                 <span class="fw-bold">Evolution</span>
+                <br />
+                <img
+                  v-if="selectedPokemon?.image"
+                  class="img-fluid evo-img"
+                  :src="require('@/assets/poke-imgs/' + selectedPokemon?.image)"
+                />
+                <i class="bi bi-arrow-right h1 ms-2 me-2"></i>
+                <img
+                  v-if="selectedPokemon?.evo1"
+                  class="img-fluid evo-img"
+                  :src="require('@/assets/poke-imgs/' + selectedPokemon?.evo1)"
+                />
+                <i class="bi bi-arrow-right h1 ms-2 me-2"></i>
+                <img
+                  v-if="selectedPokemon?.evo2"
+                  class="img-fluid evo-img"
+                  :src="require('@/assets/poke-imgs/' + selectedPokemon?.evo2)"
+                />
               </div>
             </div>
           </div>
@@ -292,5 +420,8 @@ export default {
 }
 .card-img {
   height: 210px;
+}
+.evo-img {
+  width: 80px;
 }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,62 @@
 <template>
   <div class="container">
     <Instructions title="Title" instructions="Lorum." />
+    <div class="card mb-2">
+      <div class="card-body">
+        <div class="row">
+          <div class="col text-start">
+            <div
+              class="btn-group"
+              role="group"
+              aria-label="Basic radio toggle button group"
+            >
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio1"
+                autocomplete="off"
+                checked
+              />
+              <label class="btn btn-outline-primary" for="btnradio1">A-Z</label>
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio2"
+                autocomplete="off"
+              />
+              <label class="btn btn-outline-primary" for="btnradio2">Z-A</label>
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="btnradio"
+                id="btnradio3"
+                autocomplete="off"
+              />
+              <label class="btn btn-outline-primary" for="btnradio3"
+                >Radio 3</label
+              >
+            </div>
+          </div>
+          <div class="col-4">
+            <form class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
     <PokeCard />
   </div>
 </template>

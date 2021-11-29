9ff5b68255197df9be0c147f9e3bce4c111d26fd
kenzieryann7 <makenzie_99@hotmail.com>
2021-11-28 18:03:46 -0800

    implemented filters and search bar functionality

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -1,6 +1,83 @@
 <template>
+  <div class="card mb-2">
+    <div class="card-body">
+      <div class="row">
+        <div class="col text-start">
+          <div
+            class="btn-group"
+            role="group"
+            aria-label="Basic radio toggle button group"
+          >
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio1"
+              autocomplete="off"
+              @click="getSortType('ascName')"
+            />
+            <label class="btn btn-outline-primary" for="btnradio1"
+              ><i class="bi bi-sort-alpha-down h4"></i
+            ></label>
+
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio2"
+              autocomplete="off"
+              @click="getSortType('descName')"
+            />
+            <label class="btn btn-outline-primary" for="btnradio2"
+              ><i class="bi bi-sort-alpha-up h4"></i
+            ></label>
+
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio3"
+              autocomplete="off"
+              @click="getSortType('ascID')"
+              checked
+            />
+            <label class="btn btn-outline-primary" for="btnradio3"
+              ><i class="bi bi-sort-numeric-down h4"></i
+            ></label>
+
+            <input
+              type="radio"
+              class="btn-check"
+              name="btnradio"
+              id="btnradio4"
+              autocomplete="off"
+              title="Sort ID in Descending Order"
+              @click="getSortType('descID')"
+            />
+            <label class="btn btn-outline-primary" for="btnradio4"
+              ><i class="bi bi-sort-numeric-up h4"></i
+            ></label>
+          </div>
+        </div>
+        <div class="col-4">
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="text"
+              v-model="search"
+              placeholder="Search..."
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="row">
-    <div class="col-3" v-for="poke in pokeData" :key="poke.id">
+    <div class="col-3" v-for="poke in filteredList" :key="poke.id">
       <div class="card mb-3 shadow">
         <div class="card-body">
           <img
@@ -488,10 +565,17 @@ export default {
   data: () => {
     return {
       pokeData: pokeData,
-      selectedPokemon: null
+      selectedPokemon: null,
+      search: ''
     };
   },
-  computed: {},
+  computed: {
+    filteredList() {
+      return this.pokeData.filter(poke => {
+        return poke.name.toLowerCase().includes(this.search.toLowerCase());
+      });
+    }
+  },
   methods: {
     formatId(id) {
       if (id < 10) {
@@ -505,6 +589,66 @@ export default {
     getSelectedPokemon(poke) {
       this.selectedPokemon = poke;
       console.log('Selected Pokemon:', this.selectedPokemon);
+    },
+    // FILTERS
+    getSortType(sort) {
+      let poke = this.pokeData;
+      // ascending by name
+      if (sort == 'ascName') {
+        return poke.sort((a, b) => {
+          let fa = a.name,
+            fb = b.name;
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      // descending by name
+      if (sort == 'descName') {
+        return poke.sort((a, b) => {
+          let fa = a.name,
+            fb = b.name;
+          if (fa < fb) {
+            return 1;
+          }
+          if (fa > fb) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+      // ascending by ID
+      if (sort == 'ascID') {
+        return poke.sort((a, b) => {
+          let fa = a.id,
+            fb = b.id;
+          if (fa < fb) {
+            return -1;
+          }
+          if (fa > fb) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+      // descending by ID
+      if (sort == 'descID') {
+        return poke.sort((a, b) => {
+          let fa = a.id,
+            fb = b.id;
+          if (fa > fb) {
+            return -1;
+          }
+          if (fa < fb) {
+            return 1;
+          }
+          return 0;
+        });
+      }
     }
   }
 };
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,66 +1,6 @@
 <template>
   <div class="container">
     <Instructions title="Title" instructions="Lorum." />
-    <div class="card mb-2">
-      <div class="card-body">
-        <div class="row">
-          <div class="col text-start">
-            <div
-              class="btn-group"
-              role="group"
-              aria-label="Basic radio toggle button group"
-            >
-              <input
-                type="radio"
-                class="btn-check"
-                name="btnradio"
-                id="btnradio1"
-                autocomplete="off"
-                checked
-              />
-              <label class="btn btn-outline-primary" for="btnradio1"
-                ><i class="bi bi-sort-alpha-down h4"></i
-              ></label>
-
-              <input
-                type="radio"
-                class="btn-check"
-                name="btnradio"
-                id="btnradio2"
-                autocomplete="off"
-              />
-              <label class="btn btn-outline-primary" for="btnradio2"
-                ><i class="bi bi-sort-alpha-up h4"></i
-              ></label>
-
-              <input
-                type="radio"
-                class="btn-check"
-                name="btnradio"
-                id="btnradio3"
-                autocomplete="off"
-              />
-              <label class="btn btn-outline-primary" for="btnradio3"
-                >Radio 3</label
-              >
-            </div>
-          </div>
-          <div class="col-4">
-            <form class="d-flex">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
     <PokeCard />
   </div>
 </template>
@@ -74,6 +14,10 @@ export default {
   components: {
     Instructions,
     PokeCard
-  }
+  },
+  data: () => {
+    return {};
+  },
+  methods: {}
 };
 </script>

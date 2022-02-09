17433bd3bc4a846cd22a28185bddd58ec4b23324
Kenzie <makenzie_99@hotmail.com>
2022-02-09 14:39:09 -0800

    integrated html2canvas

--- src/components/ComparePokemon.vue
+++ src/components/ComparePokemon.vue
@@ -27,7 +27,7 @@
       </div>
     </div>
     <div class="col">
-      <div class="card text-white bg-dark mb-3 shadow">
+      <div id="capture" class="card text-white bg-dark mb-3 shadow">
         <div class="card-header fw-bold">Compared Data</div>
         <div class="card-body pb-2">
           <div class="text-start mb-3">
@@ -43,24 +43,64 @@
             </h5>
           </div>
 
-          <img src="@/assets/chart.png" class="img-fluid" />Temporary Data Chart
+          <img src="@/assets/chart.png" class="img-fluid" />
         </div>
+      </div>
+      <div>
+        <button type="button" class="btn btn-primary btn-lg" @click="save">
+          Download Data
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+//import html2canvas from 'html2canvas';
+import { save } from '@/helpers/elementCapture.js';
 export default {
   name: 'ComparePokemon',
   props: {
     comparedPokemon: Array
   },
+  computed: {
+    save
+  },
   methods: {
     viewPokedex() {
       this.$emit('view-pokedex', false); // false to change the compare boolean on Main.vue to show Pokedex
     }
-  }
+    // showCaptureRef() {
+    //   console.log('Capture:', this.$refs.capture);
+    // }
+    // save: function() {
+    //   new html2canvas(this.$refs.capture, {
+    //     backgroundColor: null,
+    //     scrollX: 0,
+    //     scrollY: -window.scrollY,
+    //     scale: 1,
+    //     allowTaint: true,
+    //     useCORS: true
+    //   }).then(function(canvas) {
+    //     let uri = canvas.toDataURL();
+    //     let filename = 'data-capture';
+    //     let link = document.createElement('a');
+    //     if (typeof link.download === 'string') {
+    //       link.href = uri;
+    //       link.download = filename;
+    //       //Firefox requires the link to be in the body
+    //       document.body.appendChild(link);
+    //       //simulate click
+    //       link.click();
+    //       //remove the link when done
+    //       document.body.removeChild(link);
+    //     } else {
+    //       window.open(uri);
+    //     }
+    //   });
+    // }
+  },
+  mounted() {}
 };
 </script>
 
